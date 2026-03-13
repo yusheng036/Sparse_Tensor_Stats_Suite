0141406d55a80ffffc02f20542a2b43851fd8ebe
Yu Sheng <ays03@bu.edu>
2026-03-12 20:01:41 -0400

    Finish mnc

--- src/mnc.py
+++ src/mnc.py
@@ -4,22 +4,38 @@ from scipy import sparse
 
 @dataclass
 class StatsMNC:
-    hr: list[int]
-    hc: list[int]
-    her: list[int]
-    hec: list[int]
+    hr: np.ndarray
+    hc: np.ndarray
+    her: np.ndarray
+    hec: np.ndarray
 
 
-def density_map():
-    ...
+def mnc_stats(A: sparse.spmatrix) -> StatsMNC:
+    A = A.tocsr()
 
-def sketch(axis: str, nnz: np.ndarray) -> StatsMNC:
-    ...
+    hr = np.asarray(A.getnnz(axis=1), dtype=np.int64)
+    hc = np.asarray(A.getnnz(axis=0), dtype=np.int64)
+
+    her = np.zeros(A.shape[0], dtype=np.int64)
+    hec = np.zeros(A.shape[1], dtype=np.int64)
+
+    for i in range(A.shape[0]):
+        cols = A.indices[A.indptr[i]:A.indptr[i+1]]
+        her[i]= sum(1 for j in cols if hc[j] <= 1)
+        if len(cols) == 1:
+            hec[cols[0]] += 1
+
+    return StatsMNC(
+        hr=hr,
+        hc=hc,
+        her=her,
+        hec=hec
+    )
 
 
 def MNC(A: sparse.spmatrix, B: sparse.spmatrix) -> float:
-    hA = sketch("A", A)
-    hB = sketch("B", B)
+    hA = mnc_stats(A)
+    hB = mnc_stats(B)
     A_rows, A_cols = A.shape
     _ , B_cols = B.shape
 
@@ -33,21 +49,32 @@ def MNC(A: sparse.spmatrix, B: sparse.spmatrix) -> float:
 
     elif any(x != 0 for x in hA.hec) or any(x != 0 for x in hB.her):
         exact_nnz = sum(
-            A_hec * B_hr + (A_hc - A_hec) * B_her for A_hec, B_hr, A_hc, B_her in zip(hA.hec, hB.hr, hA.hc, hB.her)
-                    )
+            A_hec * B_hr + (A_hc - A_hec) * B_her for A_hec, A_hc, B_hr, B_her in zip(hA.hec, hA.hc, hB.hr, hB.her)
+            )
         p = (nnz_hAr- sum(1 for x in hA.hr if x == 1)) * (nnz_hBc -  sum(1 for x in hB.hc if x == 1))
 
         remaining_hA = [A_hc - A_hec for A_hc, A_hec in zip(hA.hc, hA.hec)]
         remaining_hB = [B_hr - B_her for B_hr, B_her in zip(hB.hr, hB.her)]
-        nnz = exact_nnz + density_map(remaining_hA, remaining_hB, p) * p
+        region = 0
+
+        for A_hc, B_hr in zip(remaining_hA, remaining_hB):
+            lsp = (A_hc * B_hr) / p
+            region = region + lsp - region * lsp
+
+        nnz = exact_nnz + (region * p)
 
     else:
         p = nnz_hAr * nnz_hBc
-        nnz = density_map(hA.hc, hB.hr, p) * p
+        region = 0
+
+        for A_hc, B_hr in zip(hA.hc, hB.hr):
+            lsp = (A_hc * B_hr) / p
+            region = region + lsp - region * lsp
+
+        nnz = region * p
+
 
     low_A = sum(1 for x in hA.hr if x > A_cols/2)
     low_B = sum(1 for x in hB.hc if x > A_cols/2)
     nnz = max(nnz, low_A * low_B)
     return nnz
-
-
